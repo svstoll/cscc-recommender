87b8cf73797708c4ea078b08eb6b7c16f49d17e3
svstoll <nimbatus@posteo.de>
2019-04-29 00:57:13 +0200

    Introduce functionality to collect CSCC invocation context.

--- src/main/java/ch/uzh/ifi/ase/csccrecommender/Main.java
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/Main.java
@@ -2,12 +2,12 @@ package ch.uzh.ifi.ase.csccrecommender;
 
 import cc.kave.commons.model.events.completionevents.Context;
 import ch.uzh.ifi.ase.csccrecommender.index.ContextExtractor;
+import ch.uzh.ifi.ase.csccrecommender.index.CsccContext;
 import ch.uzh.ifi.ase.csccrecommender.index.IndexCreatorVisitor;
 import com.google.inject.Guice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -20,8 +20,6 @@ public class Main {
     logger.info("Recommender launched!");
 
     List<Context> contexts = ContextExtractor.extractContextsFromZipFile(args[0]);
-    contexts.stream()
-        .findAny()
-        .ifPresent(context -> context.getSST().accept(new IndexCreatorVisitor(), new ArrayList<>()));
+    contexts.forEach(context -> context.getSST().accept(new IndexCreatorVisitor(), new CsccContext()));
   }
 }

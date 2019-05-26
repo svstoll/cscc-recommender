2dd3e77d8f7c8c4664987da2343c2af7c655b16c
Chad <347181599@qq.com>
2019-05-26 18:48:23 +0200

    evaluation package refactored

---
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/evaluation/Evaluator4Contexts.java
@@ -0,0 +1,169 @@
+package ch.uzh.ifi.ase.csccrecommender.evaluation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.name.Named;
+
+import ch.uzh.ifi.ase.csccrecommender.ProductionModule;
+import ch.uzh.ifi.ase.csccrecommender.index.MethodInvocationIndexer;
+import ch.uzh.ifi.ase.csccrecommender.properties.ConfigProperties;
+import ch.uzh.ifi.ase.csccrecommender.utility.StringUtility;
+
+public class Evaluator4Contexts {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Evaluator4Contexts.class);
+
+    private String contextsDirectoryPath;
+
+    @Inject
+    public Evaluator4Contexts(@Named(ConfigProperties.CONTEXTS_DIRECTORY_PROPERTY) String contextsDirectoryPath) {
+        this.contextsDirectoryPath = contextsDirectoryPath;
+    }
+
+    public void trainAndEvaluateOnContextDataset(int crossValidationNum) {
+        List<String[]> ds = splitContextDataset(crossValidationNum);
+        int currectItr = 0;
+        // 1. train the model
+        // mask the data to be evaluated on
+        List<String> maskedFiles = prepareContextDataset(ds.get(currectItr));
+        Injector injector = Guice.createInjector(new ProductionModule());
+        MethodInvocationIndexer methodInvocationIndexer = injector.getInstance(MethodInvocationIndexer.class);
+        methodInvocationIndexer.indexAllAvailableContexts(true);
+        // change back
+        clearup(maskedFiles);
+        maskedFiles.clear();
+        // 2. evaluate on the remaining one
+        // flip the mask
+        for (int j = 0; j < crossValidationNum; j++) {
+            if (j == currectItr)
+                continue;
+            maskedFiles.addAll(prepareContextDataset(ds.get(j)));
+        }
+        // evaluate
+        MethodInvocationRecommender methodInvocationRecommender = injector.getInstance(MethodInvocationRecommender.class);
+        methodInvocationRecommender.recommend4AllInvocationsInAvailableContexts();
+        // change back
+        clearup(maskedFiles);
+    }
+
+    public Statistics calculateStatistics(int recommendations) throws IOException {
+        File file = new File("./tmp/" + recommendations + "/");
+        Statistics statistic = new Statistics();
+        // get precision and recall
+        BufferedReader in = new BufferedReader(new FileReader("./tmp/" + recommendations + "/out.txt"));
+        String line;
+        double madeAndRelevant = 0;
+        double made = 0;
+        int requested = 0;
+        while ((line = in.readLine()) != null) {
+            String[] result = line.split(",");
+            requested += 1;
+            if (Double.parseDouble(result[0]) > 0)
+                madeAndRelevant += 1.0;
+            if (Double.parseDouble(result[1]) > 0)
+                made += 1;
+        }
+        in.close();
+        double precision = madeAndRelevant / made;
+        double recall = made / requested;
+        statistic.setPrecision(precision);
+        statistic.setRecall(recall);
+        statistic.setTotalCases(requested);
+
+        return statistic;
+    }
+
+    private List<String[]> splitContextDataset(int num) {
+        File file = new File(contextsDirectoryPath);
+        String[] directories = file.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File current, String name) {
+                return new File(current, name).isDirectory();
+            }
+        });
+        LOGGER.info("Split Context Dataset into:");
+        Collections.shuffle(Arrays.asList(directories));
+        int groupSize = directories.length / num;
+        ArrayList<String[]> list = new ArrayList<String[]>();
+        for (int i = 0; i < num; i++) {
+            String[] groupNames = new String[groupSize];
+            for (int j = 0; j < groupSize; j++) {
+                groupNames[j] = directories[i * groupSize + j];
+            }
+            LOGGER.info(i + " " + Arrays.toString(groupNames));
+            list.add(groupNames);
+        }
+        return list;
+    }
+
+    private List<String> prepareContextDataset(String[] toBeMasked) {
+        ArrayList<String> maskedZips = new ArrayList<String>();
+        for (int i = 0; i < toBeMasked.length; i++) {
+            List<String> zips = findAllZipFilePaths(contextsDirectoryPath + "/" + toBeMasked[i]);
+            for (int j = 0; j < zips.size(); j++) {
+                File file = new File(zips.get(j));
+                file.renameTo(new File(file + "mask"));
+                maskedZips.add(file + "mask");
+            }
+        }
+        return maskedZips;
+    }
+
+    private void clearup(List<String> maskedZips) {
+        for (int i = 0; i < maskedZips.size(); i++) {
+            File file = new File(maskedZips.get(i));
+            String fileName = file.getAbsolutePath();
+            file.renameTo(new File(fileName.substring(0, fileName.length() - 4)));
+        }
+    }
+
+    private static List<String> findAllZipFilePaths(String directoryPath) {
+        if (StringUtility.isNullOrEmpty(directoryPath)) {
+            return Collections.emptyList();
+        }
+
+        return FileUtils.listFiles(new File(directoryPath), new String[]{"zip"}, true)
+                .stream()
+                .map(File::getAbsolutePath)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        int crossValidationNum = 2;
+        Injector injector = Guice.createInjector(new ProductionModule());
+        Evaluator4Contexts evaluator = injector.getInstance(Evaluator4Contexts.class);
+        evaluator.trainAndEvaluateOnContextDataset(crossValidationNum);
+
+        int[] recommendationNum = {1, 3, 10};
+        for (int rNum : recommendationNum) {
+            System.out.println("MAX_RECOMMENDATIONS = " + rNum);
+            Statistics statistic = null;
+            try {
+                statistic = evaluator.calculateStatistics(rNum);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(statistic.getPrecision() + "," + statistic.getRecall() + "," + statistic.getF_Measure() + "," + statistic.getTotalCases());
+
+        }
+
+
+    }
+
+}
---
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/evaluation/R4InvocationLineContextVisitor.java
@@ -0,0 +1,80 @@
+package ch.uzh.ifi.ase.csccrecommender.evaluation;
+
+import ch.uzh.ifi.ase.csccrecommender.index.MethodInvocationIndex;
+import ch.uzh.ifi.ase.csccrecommender.mining.CsccContext;
+import ch.uzh.ifi.ase.csccrecommender.mining.RecommendingLineContextVisitor;
+import ch.uzh.ifi.ase.csccrecommender.recommender.RecommendationResult;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class R4InvocationLineContextVisitor extends RecommendingLineContextVisitor {
+
+    public R4InvocationLineContextVisitor(MethodInvocationIndex methodInvocationIndex) {
+        super(methodInvocationIndex);
+    }
+
+    @Override
+    protected void handleMethodInvocation(String methodName, String invocationType, CsccContext csccContext) {
+        super.handleCompletionExpression(invocationType, csccContext);
+        // record in txt
+        String correctRecommendation = methodName;
+        String isRecommendationCorrectFor1 = "0";
+        String isRecommendationCorrectFor3 = "0";
+        String isRecommendationCorrectFor10 = "0";
+
+        List<String> recommendations = super.getRecommendationResults().get(0).getRecommendedMethods();
+        Double recallFor1 = recommendations.size() / 1.0;
+        Double recallFor3 = recommendations.size() / 3.0;
+        Double recallFor10 = recommendations.size() / 10.0;
+        int i = 0;
+        for (String recommendation : recommendations) {
+            if (recommendation.equalsIgnoreCase(correctRecommendation)) {
+                isRecommendationCorrectFor10 = "1";
+                if (i < 1) {
+                    isRecommendationCorrectFor1 = "1";
+                    isRecommendationCorrectFor3 = "1";
+                } else if (i < 3)
+                    isRecommendationCorrectFor3 = "1";
+
+            }
+            i++;
+        }
+        PrintWriter pw;
+        try {
+            // 1
+            File directory = new File("./tmp/1/");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            pw = new PrintWriter(new FileWriter("./tmp/1/out.txt", true));
+            pw.println(isRecommendationCorrectFor1 + "," + recallFor1.toString());
+            pw.close();
+            // 3
+            directory = new File("./tmp/3/");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            pw = new PrintWriter(new FileWriter("./tmp/3/out.txt", true));
+            pw.println(isRecommendationCorrectFor3 + "," + recallFor3.toString());
+            pw.close();
+            // 10
+            directory = new File("./tmp/10/");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            pw = new PrintWriter(new FileWriter("./tmp/10/out.txt", true));
+            pw.println(isRecommendationCorrectFor10 + "," + recallFor10.toString());
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return;
+
+    }
+
+}

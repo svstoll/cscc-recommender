53b90e35cb1d249048b905d34b516a4e93903167
Chad <347181599@qq.com>
2019-05-30 10:03:35 +0200

    fixed some issues; clear tmp folder; store results in a file

--- src/main/java/ch/uzh/ifi/ase/csccrecommender/evaluation/Statistics.java
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/evaluation/Statistics.java
@@ -1,40 +1,38 @@
 package ch.uzh.ifi.ase.csccrecommender.evaluation;
 
-public class Statistics {
+class Statistics {
 
 
     private int totalCases;
     private double Precision;
     private double Recall;
-    private double F_Measure;
 
-    public int getTotalCases() {
+    int getTotalCases() {
         return totalCases;
     }
 
-    public void setTotalCases(int totalCases) {
+    void setTotalCases(int totalCases) {
         this.totalCases = totalCases;
     }
 
-    public double getPrecision() {
+    double getPrecision() {
         return Precision;
     }
 
-    public void setPrecision(double precision) {
+    void setPrecision(double precision) {
         Precision = precision;
     }
 
-    public double getRecall() {
+    double getRecall() {
         return Recall;
     }
 
-    public void setRecall(double recall) {
+    void setRecall(double recall) {
         Recall = recall;
     }
 
-    public double getF_Measure() {
-        F_Measure = 2 * Precision * Recall / (Precision + Recall);
-        return F_Measure;
+    double getF_Measure() {
+        return 2 * Precision * Recall / (Precision + Recall);
     }
 
 

ee27c927be29840d5439f7e0f5b0676920e1d87a
Sven Stoll <s.stoll@posteo.de>
2019-05-31 11:46:16 +0200

    Change license, add license headers and update project information.

--- src/main/java/com/github/svstoll/csccrecommender/utility/FileUtility.java
+++ src/main/java/com/github/svstoll/csccrecommender/utility/FileUtility.java
@@ -1,8 +1,26 @@
+/*
+ *  Copyright 2019 Sven Stoll, Dingguang Jin, Tran Phan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.github.svstoll.csccrecommender.utility;
 
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,7 +31,7 @@ public class FileUtility {
   }
 
   public static List<String> findAllZipFilePaths(String directoryPath) {
-    if (StringUtility.isNullOrEmpty(directoryPath)) {
+    if (StringUtility.isNullOrEmpty(directoryPath) || !Files.exists(Paths.get(directoryPath))) {
       return Collections.emptyList();
     }
 
